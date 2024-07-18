20fc9c1480f6b8bb477e2f98b028f031bea1764d
EzequielvRS <valentinreyessal@gmail.com>
2024-07-18 09:55:38 -0600

    Nueva interfaz

--- app/src/main/java/com/example/nutriwatch4/seguimiento_al.java
+++ app/src/main/java/com/example/nutriwatch4/seguimiento_al.java
@@ -1,8 +1,6 @@
 package com.example.nutriwatch4;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Confirm_Register extends AppCompatActivity {
+public class seguimiento_al extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_confirm_register);
+        setContentView(R.layout.activity_seguimiento_al);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void MenuP (View view){
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
     }
 }
