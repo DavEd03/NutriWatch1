41297202100e2844c2d4d7b8b324b66a123acb4d
DavEd03 <david.edu.cisne.2d@gmail.com>
2024-07-26 22:12:11 -0600

    Added Login and Password

--- app/src/main/java/com/example/nutriwatch4/register.java
+++ app/src/main/java/com/example/nutriwatch4/register.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class register extends AppCompatActivity {
     FirebaseAuth mAuth;
@@ -27,6 +28,7 @@ public class register extends AppCompatActivity {
     EditText edad;
     EditText ciudad;
     Button regis;
+    String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,17 +49,26 @@ public class register extends AppCompatActivity {
     public void Register (View view){
         try {
         mAuth= FirebaseAuth.getInstance();
-        String Nombres = (String) nombre.getText().toString().trim();
-        String Apellidos = (String)contra.getText().toString().trim();
-        if(Nombres.isEmpty()&& Apellidos.isEmpty()){
+        String Correo = (String) email.getText().toString().trim();
+        String Contra = (String)contra.getText().toString().trim();
+        if(Correo.isEmpty()&& Contra.isEmpty()){
             Toast.makeText(register.this, "Ingresa los datos correctos", Toast.LENGTH_LONG).show();
         }else{
-            mAuth.createUserWithEmailAndPassword(Nombres,Apellidos).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(Correo,Contra).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isComplete()){
-                        Toast.makeText(register.this,"Usuario registrado exitosamente",Toast.LENGTH_LONG).show();
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if (user != null) {
+                             uid = user.getUid();
+                            Toast.makeText(register.this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(register.this, "EL usuario no existe", Toast.LENGTH_SHORT).show();
+                        }
+
+
                     }
+
                 }
             }).addOnFailureListener(this, new OnFailureListener() {
                 @Override
@@ -69,8 +80,13 @@ public class register extends AppCompatActivity {
         }catch (Exception e){
             Toast.makeText(register.this,"Error "+e.getMessage().toString(),Toast.LENGTH_LONG).show();
         }
+        try{
+            String Nombre= nombre.getText().toString().trim();
+            String Edad= edad.getText().toString().trim();
+            String Ciudad= ciudad.getText().toString().trim();
+            String Correo= email.getText().toString().trim();
+        }catch (Exception e){
 
-
-
+        }
     }
 }
