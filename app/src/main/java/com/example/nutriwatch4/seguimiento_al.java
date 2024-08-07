fd2ad9660fcf8c73d2655618bc5aa3c4bb484ea8
EzequielvRS <valentinreyessal@gmail.com>
2024-08-07 09:48:18 -0600

    se agrego una plantilla de recomendaciones de ejercisios y agrewgaron los button y sus respectivos metodos de cada uno en todas las paginas o plantillas

--- app/src/main/java/com/example/nutriwatch4/seguimiento_al.java
+++ app/src/main/java/com/example/nutriwatch4/seguimiento_al.java
@@ -1,6 +1,8 @@
 package com.example.nutriwatch4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class seguimiento_al extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void MenuP (View view){
+        Intent i = new Intent(this, Menu_Principal.class);
+        startActivity(i);
     }
 }
