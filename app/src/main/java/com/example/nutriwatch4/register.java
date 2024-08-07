1bf5eeae2f331386c329ccea6e32a5426564a141
EzequielvRS <valentinreyessal@gmail.com>
2024-08-07 09:53:42 -0600

    se agrego una plantilla de recomendaciones de ejercisios y agrewgaron los button y sus respectivos metodos

--- app/src/main/java/com/example/nutriwatch4/register.java
+++ app/src/main/java/com/example/nutriwatch4/register.java
@@ -37,7 +37,7 @@ public class register extends AppCompatActivity {
         email=(EditText)findViewById(R.id.correo);
         edad=(EditText)findViewById(R.id.fec_nac);
         ciudad=(EditText)findViewById(R.id.city);
-        regis=(Button) findViewById(R.id.regis);
+        regis=(Button) findViewById(R.id.REGRESAR);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.seguimient_al), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
